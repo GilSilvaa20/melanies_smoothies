bf07acca3c099002921154efdf1d364a7587eafb
GilSilvaa20 <117319767+GilSilvaa20@users.noreply.github.com>
2024-12-24 14:26:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,6 @@ st.write(
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
-cnx = st.connection("snowflake")
-session = cnx.session()
 
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')) # With the select(col()) I can specify which columns to extract from a table.
